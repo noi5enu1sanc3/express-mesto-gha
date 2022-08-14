74b98f703849fd69f4189e7dc312c707ef0d2d0d
noisenuisance <sabauspne@gmail.com>
2022-08-14 03:08:31 +0300

    add models user and card

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);

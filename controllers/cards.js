6edc1168dc428ec54319fd6a03f7e7af900d8283
noisenuisance <sabauspne@gmail.com>
2022-08-25 07:36:06 +0300

    refactor controllers to async/await

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,72 +3,100 @@ const Card = require('../models/card');
 const CardNotFoundError = require('../helpers/errors/CardNotFoundError');
 const ValidationError = require('../helpers/errors/ValidationError');
 
-const getCards = (req, res, next) => {
-  Card.find({})
-    .populate('owner')
-    .then((cards) => res.send({ data: cards }))
-    .catch((err) => next(err));
+const getCards = async (req, res, next) => {
+  try {
+    const cards = await Card.find({})
+      .populate('owner');
+    res.send({ data: cards });
+  } catch (err) {
+    next(err);
+  }
 };
 
-const createCard = (req, res, next) => {
+const createCard = async (req, res, next) => {
   const { name, link } = req.body;
-  Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => (err instanceof mongoose.Error.ValidationError
-      ? next(new ValidationError(`Validation error: ${err.message}`))
-      : next(err)));
+  try {
+    const card = await Card.create({ name, link, owner: req.user._id });
+    res.send({ data: card });
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      next(new ValidationError(`Validation error: ${err.message}`));
+    } else {
+      next(err);
+    }
+  }
 };
 
-const deleteCard = (req, res, next) => {
-  Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => (!card ? next(new CardNotFoundError()) : res.send({ data: card })))
-    .catch((err) => (err instanceof mongoose.Error.CastError
-      ? next(new ValidationError(`Validation error: ${err.message}`))
-      : next(err)));
+const deleteCard = async (req, res, next) => {
+  try {
+    const card = await Card.findByIdAndRemove(req.params.cardId);
+
+    if (!card) {
+      next(new CardNotFoundError());
+    } else {
+      res.send({ data: card });
+    }
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      next(new ValidationError(`Validation error: ${err.message}`));
+    } else {
+      next(err);
+    }
+  }
 };
 
-const likeCard = (req, res, next) => {
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: req.user._id } },
-    {
-      new: true,
-      runValidators: true,
-    },
-  )
-    .populate('likes')
-    .then((card) => (!card ? next(new CardNotFoundError()) : res.send({ data: card })))
-    .catch((err) => {
-      if (
-        err instanceof mongoose.Error.ValidationError
-        || err instanceof mongoose.Error.CastError) {
-        next(new ValidationError(`Validation error: ${err.message}`));
-      } else {
-        next(err);
-      }
-    });
+const likeCard = async (req, res, next) => {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } },
+      {
+        new: true,
+        runValidators: true,
+      },
+    )
+      .populate('likes');
+    if (!card) {
+      next(new CardNotFoundError());
+    } else {
+      res.send({ data: card });
+    }
+  } catch (err) {
+    if (
+      err instanceof mongoose.Error.ValidationError
+      || err instanceof mongoose.Error.CastError) {
+      next(new ValidationError(`Validation error: ${err.message}`));
+    } else {
+      next(err);
+    }
+  }
 };
 
-const dislikeCard = (req, res, next) => {
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: req.user._id } },
-    {
-      new: true,
-      runValidators: true,
-    },
-  )
-    .populate('likes')
-    .then((card) => (!card ? next(new CardNotFoundError()) : res.send({ data: card })))
-    .catch((err) => {
-      if (
-        err instanceof mongoose.Error.ValidationError
-        || err instanceof mongoose.Error.CastError) {
-        next(new ValidationError(`Validation error: ${err.message}`));
-      } else {
-        next(err);
-      }
-    });
+const dislikeCard = async (req, res, next) => {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      {
+        new: true,
+        runValidators: true,
+      },
+    )
+      .populate('likes');
+    if (!card) {
+      next(new CardNotFoundError());
+    } else {
+      res.send({ data: card });
+    }
+  } catch (err) {
+    if (
+      err instanceof mongoose.Error.ValidationError
+      || err instanceof mongoose.Error.CastError) {
+      next(new ValidationError(`Validation error: ${err.message}`));
+    } else {
+      next(err);
+    }
+  }
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -3,59 +3,88 @@ const UserNotFoundError = require('../helpers/errors/UserNotFoundError');
 const ValidationError = require('../helpers/errors/ValidationError');
 const User = require('../models/user');
 
-const getUsers = (req, res, next) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch((err) => next(err));
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    res.send({ data: users });
+  } catch (err) {
+    next(err);
+  }
 };
 
-const findUserById = (req, res, next) => {
-  User.findById(req.params.userId)
-    .then((user) => (!user ? next(new UserNotFoundError()) : res.send({ data: user })))
-    .catch((err) => (err instanceof mongoose.Error.CastError
-      ? next(new ValidationError())
-      : next(err)));
+const findUserById = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      next(new UserNotFoundError());
+    } else {
+      res.send({ data: user });
+    }
+  } catch (err) {
+    if (err instanceof mongoose.Error.CastError) {
+      next(new ValidationError(`Validation error: ${err.message}`));
+    } else {
+      next(err);
+    }
+  }
 };
 
-const createUser = (req, res, next) => {
+const createUser = async (req, res, next) => {
   const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => (err instanceof mongoose.Error.ValidationError
-      ? next(new ValidationError(`Validation error: ${err.message}`))
-      : next(err)));
+  try {
+    const user = await User.create({ name, about, avatar });
+    res.send({ data: user });
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      next(new ValidationError(`Validation error: ${err.message}`));
+    } else {
+      next(err);
+    }
+  }
 };
 
-const updateUserProfile = (req, res, next) => {
+const updateUserProfile = async (req, res, next) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, {
-    new: true,
-    runValidators: true,
-  }).then((user) => (!user ? next(new UserNotFoundError()) : res.send({ data: user })))
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError
-        || err instanceof mongoose.Error.CastError) {
-        next(new ValidationError(`Validation error: ${err.message}`));
-      } else {
-        next(err);
-      }
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, { name, about }, {
+      new: true,
+      runValidators: true,
     });
+    if (!user) {
+      next(new UserNotFoundError());
+    } else {
+      res.send({ data: user });
+    }
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError
+      || err instanceof mongoose.Error.CastError) {
+      next(new ValidationError(`Validation error: ${err.message}`));
+    } else {
+      next(err);
+    }
+  }
 };
 
-const updateUserAvatar = (req, res, next) => {
+const updateUserAvatar = async (req, res, next) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, {
-    new: true,
-    runValidators: true,
-  }).then((user) => (!user ? next(new UserNotFoundError()) : res.send({ data: user })))
-    .catch((err) => {
-      if (err instanceof mongoose.Error.ValidationError
-        || err instanceof mongoose.Error.CastError) {
-        next(new ValidationError(`Validation error: ${err.message}`));
-      } else {
-        next(err);
-      }
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, { avatar }, {
+      new: true,
+      runValidators: true,
     });
+    if (!user) {
+      next(new UserNotFoundError());
+    } else {
+      res.send({ data: user });
+    }
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError
+      || err instanceof mongoose.Error.CastError) {
+      next(new ValidationError(`Validation error: ${err.message}`));
+    } else {
+      next(err);
+    }
+  }
 };
 
 module.exports = {

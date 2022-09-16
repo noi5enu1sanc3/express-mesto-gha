787c52eff0253cc62291f8157afe99d8848449d5
noisenuisance <sabauspne@gmail.com>
2022-09-16 03:00:05 +0300

    add cookie sameSite attr

--- controllers/users.js
+++ controllers/users.js
@@ -25,6 +25,7 @@ const login = async (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'none',
+        secure: true,
       })
       .send({ data: user.toJSON() });
   } catch (err) {
